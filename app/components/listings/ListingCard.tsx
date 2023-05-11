97abe3ba24a016f2750976d70fab27c0a9cd358a
Karl Behrens <karlbehrensg@gmail.com>
2023-05-11 10:54:17 -0400

    Listing individual view

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -5,14 +5,14 @@ import useCountries from "@/app/hooks/useCountries";
 import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 
-import { SafeUser } from "@/app/types";
-import { Listing, Reservation } from "@prisma/client";
+import { SafeListing, SafeUser } from "@/app/types";
+import { Reservation } from "@prisma/client";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
