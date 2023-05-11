3e7c4a3d6ea6b0a26b2b931c789434295a9fc440
Karl Behrens <karlbehrensg@gmail.com>
2023-05-11 18:44:30 -0400

    Reservation screen

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -39,7 +39,9 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
+        toast.success("Account created!");
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((err) => {
         toast.error("Something went wrong!");
