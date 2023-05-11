af443c7f5187491d7ee0838e215f2de2a81ea09e
Karl Behrens <karlbehrensg@gmail.com>
2023-05-11 17:40:38 -0400

    Trip screen

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -5,15 +5,14 @@ import useCountries from "@/app/hooks/useCountries";
 import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 
-import { SafeListing, SafeUser } from "@/app/types";
-import { Reservation } from "@prisma/client";
+import { SafeListing, SafeReservation, SafeUser } from "@/app/types";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
   data: SafeListing;
-  reservation?: Reservation;
+  reservation?: SafeReservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
   actionLabel?: string;
